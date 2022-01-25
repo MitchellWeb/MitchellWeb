73123e96f89051837fa133bb54dbfb42204e7111
MitchellWeb <mitchell.nesheim12@gmail.com>
2022-01-25 15:18:04 -0600

    hello world test commit

---
+++ helloworld.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WDV341 Intro to PHP</title>
+</head>
+
+<body>
+    <h1>WDV341 Intro to PHP</h1>
+    <h2>Mitchell Nesheim</h2>
+    <?php echo "<h3>Hello World! from PHP</h3>"; ?>
+</body>
+
+</html>
